85bf3b78d45bc54d8906ca11bb442cb7edc73e08
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-09-25 19:33:05 +0500

    Task2

---
+++ Tyuiu.KozhevnikovDG.Sprint1.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KozhevnikovDG.Sprint1.Task2.V8.Lib;
+
+namespace Tyuiu.KozhevnikovDG.Sprint1.Task2.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 2;
+            int b = 4;
+            var res = ds.CalculatePerimetr(a, b);
+            Assert.AreEqual(12, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovDG.Sprint1.Task2.V8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KozhevnikovDG.Sprint1.Task2.V8.Lib;
+
+namespace Tyuiu.KozhevnikovDG.Sprint1.Task2.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Кожевников Д. Г. | ИИПБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #8                                                             *");
+            Console.WriteLine("* Выполнил: Кожевников Данил Георгиевич | ИИПБ-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные  *");
+            Console.WriteLine("* выполняет указанные расчеты и печатает результат на экране              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int a;
+            Console.WriteLine("Введите значение a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int b;
+            Console.WriteLine("Введите значение b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Периметр прямоугольника = " + ds.CalculatePerimetr(a, b));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
